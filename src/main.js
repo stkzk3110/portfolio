e0df28c985558bbb64b5a3113c6308f3bd2078d7
fisherman0825 <fisherman0825dd@gmail.com>
2019-02-23 14:08:47 +0900

    bootstrap introduce

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import 'shitajicss/docs/css/shitaji.min.css'
 import './scss/style.scss'
+import './scss/custom.scss'
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import router from './router'
 import App from './App.vue'
+
+Vue.use(BootstrapVue)
 
 new Vue({
   el: '#app',
